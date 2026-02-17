51b07970fba0c21547315655597a595b1db7fe9d
Young-Chung Hsue <hsuy1234@hotmail.com>
2026-02-17 19:58:37 +0800

    再試一次for iPhone

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig(({ command }) => ({
         display: 'standalone',
         background_color: '#0f172a',
         theme_color: '#0f172a',
+        orientation: 'portrait-primary',
         icons: [
           {
             src: 'icons/icon-192.svg',
